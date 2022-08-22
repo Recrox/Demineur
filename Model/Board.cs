668831cd534bada036490a11727b0ad281f8a3b5
Arno Leruste <recroX91@hotmail.com>
2022-08-22 12:08:28 +0200

    Ajoutez des fichiers projet.

---
+++ Model/Board.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demineur.Model
+{
+    internal class Board
+    {
+        public const int X_SIZE = 10;
+        public const int Y_SIZE = 10;
+        Cell [,] board = new Cell[X_SIZE, Y_SIZE];
+
+        public Board()
+        {
+            FillBoard();
+        }
+
+        private void FillBoard()
+        {
+            for (int i = 0; i < X_SIZE; i++)
+            {
+                for (int y = 0; y < Y_SIZE; y++)
+                {
+                    this.board[i,y] = new Cell();
+                }
+            }
+        }
+    }
+}
---
+++ Model/Cell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demineur.Model
+{
+    internal class Cell
+    {
+        bool hasBomb;
+        bool isDiscover;
+
+        public Cell(bool hasBomb)
+        {
+            this.hasBomb = hasBomb;
+        }
+
+
+    }
+}
